c3b87c4fc19481d41d4b7242e21502c694d93780
L <L@my.com>
2023-08-01 16:13:35 +0800

    renamed

--- mall-member/src/main/java/com/mall/member/config/MybatisPlusConfig.java
+++ mall-member/src/main/java/com/mall/member/config/MybatisPlusConfig.java
@@ -1,4 +1,4 @@
-package com.mall.order.config;
+package com.mall.member.config;
 
 import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
@@ -17,8 +17,8 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
  */
 @Configuration
 @EnableTransactionManagement
-@MapperScan("com.mall.order.dao")
-public class MyBatisConfig {
+@MapperScan("com.mall.member.dao")
+public class MybatisPlusConfig {
 
     /**
      * 新的分页插件,一缓和二缓遵循mybatis的规则,需要设置 MybatisConfiguration#useDeprecatedExecutor = false 避免缓存出现问题(该属性会在旧插件移除后一同移除)
