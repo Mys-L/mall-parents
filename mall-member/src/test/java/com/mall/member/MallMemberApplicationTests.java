90b7b6363679268377ddef6713b00286e8b79ea1
L <L@my.com>
2023-10-23 16:31:50 +0800

    认证服务 短信验证 验证码防刷 异常 密码加密

--- mall-member/src/test/java/com/mall/member/MallMemberApplicationTests.java
+++ mall-member/src/test/java/com/mall/member/MallMemberApplicationTests.java
@@ -1,13 +1,28 @@
 package com.mall.member;
 
+import org.apache.commons.codec.digest.DigestUtils;
+import org.apache.commons.codec.digest.Md5Crypt;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootTest
 class MallMemberApplicationTests {
 
     @Test
     void contextLoads() {
+
+        String md5Hex = DigestUtils.md5Hex("123456");
+
+        String s = Md5Crypt.md5Crypt("123456".getBytes());
+
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        String encoded = passwordEncoder.encode("123456");
+        boolean matches = passwordEncoder.matches("123456", "$2a$10$3gUQvdg9sWsHC9FDwAAFWOAsdPVdiB92M38WO4XkPVWdD7MJrgEa2");
+        System.out.println(matches);
+        System.out.println(encoded);
+        System.out.println(md5Hex);
+        System.out.println(s);
     }
 
 }
